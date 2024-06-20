9e98c6585e18bb727cdfbcfcce7ddbca8e9384de
Yelin Jeong <yelini.jeong@samsung.com>
2024-06-20 15:01:03 +0900

    s
    link
    
    test
    
    test
    
    test
    
    test
    
    s

--- jni/Android-nnstreamer.mk
+++ jni/Android-nnstreamer.mk
@@ -5,15 +5,15 @@ LOCAL_PATH := $(call my-dir)
 #
 # Step1: Required packages
 # - Software Platform: Android 7.0 (Nougat) + ARM 64bit
-#   - NDK version: r12b
-#   - API Level: 24
-# - Gstreamer 1.12
+#   - NDK version: r25c
+#   - API Level: 33
+# - Gstreamer 1.24
 #
 # Step2: Append a path of Android Gstreamer and Android NDK as following:
 # vi ~/.bashrc
 # # gst-root-android, Android NDK/SDK
 # export GSTREAMER_ROOT_ANDROID=/work/taos/gst-android/gst_root_android
-# export ANDROID_NDK=/work/taos/gst-android/android-ndk-r12b
+# export ANDROID_NDK=/work/taos/gst-android/android-ndk-r25c
 # export PATH=$ANDROID_NDK:$PATH
 #
 # Step3: Build NNStreamer for Android platform
@@ -48,50 +48,31 @@ else
 $(error Target arch ABI not supported: $(TARGET_ARCH_ABI))
 endif
 
+GSTREAMER_NDK_BUILD_PATH := $(GSTREAMER_ROOT)/share/gst-android/ndk-build
+include $(LOCAL_PATH)/Android-gst-plugins.mk
+
+GST_BLOCKED_PLUGINS      := \
+        fallbackswitch livesync rsinter rstracers \
+        threadshare togglerecord cdg claxon dav1d rsclosedcaption \
+        ffv1 fmp4 mp4 gif hsv lewton rav1e json rspng regex textwrap textahead \
+        aws hlssink3 ndi rsonvif raptorq reqwest rsrtp rsrtsp webrtchttp rswebrtc uriplaylistbin \
+        rsaudiofx rsvideofx
+
+GSTREAMER_PLUGINS        := $(filter-out $(GST_BLOCKED_PLUGINS), $(GST_REQUIRED_PLUGINS))
+GSTREAMER_EXTRA_DEPS     := $(GST_REQUIRED_DEPS) glib-2.0 gio-2.0 gmodule-2.0
+GSTREAMER_EXTRA_LIBS     := $(GST_REQUIRED_LIBS) -liconv
+
+ifeq ($(NNSTREAMER_API_OPTION),all)
+GSTREAMER_EXTRA_LIBS += -lcairo
+endif
+
+GSTREAMER_INCLUDE_FONTS := no
+GSTREAMER_INCLUDE_CA_CERTIFICATES := no
+
+include $(GSTREAMER_NDK_BUILD_PATH)/gstreamer-1.0.mk
+
 # Common definition for NNStreamer
 include $(LOCAL_PATH)/nnstreamer.mk
-
-# Define shared libraries that are required by a gstreamer plug-in.
-define shared_lib_common
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := $(1)
-    LOCAL_SRC_FILES := $(GSTREAMER_ROOT)/lib/lib$(1).so
-    include $(PREBUILT_SHARED_LIBRARY)
-endef
-
-# Define shared libraries that are used as a gstreamer plug-in.
-define shared_lib_gst
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := $(1)
-    LOCAL_SRC_FILES := $(GSTREAMER_ROOT)/lib/gstreamer-1.0/lib$(1).so
-    include $(PREBUILT_SHARED_LIBRARY)
-endef
-
-# Describe shared libraries that are needed to run this application.
-
-so_names_common := gstreamer-1.0 gstbase-1.0 gstvideo-1.0 glib-2.0 \
-                   gobject-2.0 intl z bz2 orc-0.4 gmodule-2.0 ffi gsttag-1.0 iconv \
-                   gstapp-1.0 png16 gstbadbase-1.0 gio-2.0 pangocairo-1.0 \
-                   pangoft2-1.0 pango-1.0 gthread-2.0 cairo pixman-1 fontconfig expat freetype \
-                   gstbadvideo-1.0 gstcontroller-1.0 jpeg graphene-1.0 gstpbutils-1.0 gstgl-1.0 \
-                   gstallocators-1.0 gstbadallocators-1.0 harfbuzz
-
-ifeq ($(NO_AUDIO), false)
-so_names_common += gstaudio-1.0 gstbadaudio-1.0
-endif
-
-$(foreach item,$(so_names_common),$(eval $(call shared_lib_common,$(item))))
-
-so_names_gst := gstcoreelements gstcoretracers gstadder gstapp \
-                gstpango gstrawparse gsttypefindfunctions gstvideoconvert gstvideorate \
-                gstvideoscale gstvideotestsrc gstvolume gstautodetect gstvideofilter gstopengl \
-                gstopensles gstcompositor gstpng gstmultifile
-
-ifeq ($(NO_AUDIO), false)
-so_names_gst += gstaudioconvert gstaudiomixer gstaudiorate gstaudioresample gstaudiotestsrc
-endif
-
-$(foreach item,$(so_names_gst),$(eval $(call shared_lib_gst,$(item))))
 
 include $(CLEAR_VARS)
 
@@ -106,8 +87,7 @@ LOCAL_CFLAGS += -DNO_AUDIO
 LOCAL_CXXFLAGS += -DNO_AUDIO
 endif
 
-LOCAL_LDLIBS        := -llog
-LOCAL_LDFLAGS       += -fuse-ld=bfd
+LOCAL_LDLIBS        := -llog -landroid
 LOCAL_MODULE_TAGS   := optional
 
 LOCAL_MODULE        := nnstreamer
@@ -128,6 +108,6 @@ endif
 
 LOCAL_C_INCLUDES += $(GST_HEADERS_COMMON)
 
-LOCAL_SHARED_LIBRARIES := $(BUILDING_BLOCK_LIST)
+LOCAL_SHARED_LIBRARIES := $(BUILDING_BLOCK_LIST) gstreamer_android
 
 include $(BUILD_SHARED_LIBRARY)
